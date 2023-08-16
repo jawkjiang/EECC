c505714759920d84874b4e0164e0a85161e877f6
蒋夏雨 <65795647+jawkjiang@users.noreply.github.com>
2023-08-16 10:14:17 +0800

    Add files via upload

--- algorithm0.py
+++ algorithm0.py
@@ -2,14 +2,13 @@
 
 import csv
 import numpy as np
-import matlab.engine
+import matlab
 from sko.GA import GA
 from sko.PSO import PSO
 from sko.DE import DE
 from sko.SA import SA
 from sko.AFSA import AFSA
 from sko.tools import set_run_mode
-import matplotlib.pyplot
 
 
 def loss_read(path):
@@ -32,18 +31,8 @@ def station_read(path):
     return station_list
 
 
-def linear_planning(provice):
-    # 调用MATLAB代码
-    eng = matlab.engine.start_matlab()
-    eng.cd(f'data/{provice}/main/main_tf')
-    # 运行MATLAB代码，结果存储在MATLAB的变量P_result_all中，为96*i的矩阵，类型为matlab.double，需要转换为list
-    P_result_all = eng.main_tf(nargout=1)
-    P_result_all = list(P_result_all)
-    # 将P_result_all转换为list，reform为i*96的矩阵
-    best_x = []
-    for i in range(len(P_result_all)):
-        best_x.append(list(P_result_all[i]))
-    print(best_x)
+def linear_planning():
+    pass
 
 
 class AI_planing:
@@ -73,40 +62,44 @@ class AI_planing:
                     # 氢能源出力序列
                     globals()[f'x{i}_{j}'] = x[i * 96 + j]
                     # 氢能源容量序列
-                    globals()[f'x{i + len(self.H2_power)}_{j}'] = x[96 * len(self.H2_power) + i * 96 + j]
 
             for i in range(len(self.ele_power)):
                 for j in range(96):
                     # 电化学出力序列
-                    globals()[f'y{i}_{j}'] = x[96 * 2 * len(self.H2_power) + i * 96 + j]
+                    globals()[f'y{i}_{j}'] = x[96 * len(self.H2_power) + i * 96 + j]
                     # 电化学容量序列
-                    globals()[f'y{i + len(self.ele_power)}_{j}'] = x[
-                        96 * 2 * len(self.H2_power) + 96 * len(self.ele_power) + i * 96 + j]
 
             for i in range(len(self.water_power)):
                 for j in range(96):
                     # 抽水蓄能出力序列
-                    globals()[f'z{i}_{j}'] = x[96 * 2 * len(self.H2_power) + 96 * 2 * len(self.ele_power) + i * 96 + j]
+                    globals()[f'z{i}_{j}'] = x[96 * len(self.H2_power) + 96 * len(self.ele_power) + i * 96 + j]
                     # 抽水蓄能容量序列
-                    globals()[f'z{i + len(self.water_power)}_{j}'] = x[
-                        96 * 2 * len(self.H2_power) + 96 * 2 * len(self.ele_power) +
-                        96 * len(self.water_power) + i * 96 + j]
 
             return \
                     sum(eval(f"max(1.89 * x{i}_{j}, -1.3 * x{i}_{j})") for i in range(len(self.H2_power)) for j in
                         range(96)) + \
+\
  \
                     2.94 * sum([eval(f"abs(y{i}_{j})") for i in range(len(self.ele_power)) for j in range(96)]) \
-                    + 102e4 * sum([(3e-4 - 0.009 * (eval(f"y{i + len(self.ele_power)}_{j}") / 60 / self.ele_power[i]
-                                                    - (1 - 1 / 3000) ** (1 / 2.66) * self.ele_power[i] / 60 / 15)) *
-                                   self.ele_power[
-                                       i] / 365 / 96 / 1e3
-                                   for i in range(len(self.ele_power)) for j in range(96)]) + \
+\
+                    + 102e4 * sum([(
+                                           3e-4 - 0.009 * (self.ele_power[i] / 60 / self.ele_power[i]
+                                                            - (1 - 1 / 3000) ** (1 / 2.66) * self.ele_power[i] / 60 / 15))
+                                   * self.ele_power[i] / 365 / 96 / 1e3 for i in range(len(self.ele_power))]) \
  \
+                    + 102e4 * sum([(
+                                           3e-4 - 0.009 * ((self.ele_power[i]
+                                                           - 0.25 * sum(eval(f"max(y{i}_{l}, 0.9 * y{i}_{l})") for l in range(j)))
+                                                            / 60 / self.ele_power[i]
+                                            - (1 - 1 / 3000) ** (1 / 2.66) * self.ele_power[i] / 60 / 15))
+                                   * self.ele_power[i] / 365 / 96 / 1e3
+                                   for i in range(len(self.ele_power)) for j in range(1, 96)]) + \
+ \
+\
                     100 * sum(eval(f"abs(z{i}_{j})") for i in range(len(self.water_power)) for j in range(96))
 
         self.obj_func = obj_func
-        self.n_dim = 2 * 96 * (len(self.H2_power) + len(self.ele_power) + len(self.water_power))
+        self.n_dim = 96 * (len(self.H2_power) + len(self.ele_power) + len(self.water_power))
 
         # 约束条件建模
         # 上下限约束建模
@@ -116,26 +109,16 @@ class AI_planing:
             for j in range(96):
                 self.ub.append(self.H2_power[i])
                 self.lb.append(-self.H2_power[i])
-        for i in range(len(self.H2_power)):
-            for j in range(96):
-                self.ub.append(5893673.32211)
-                self.lb.append(0)
         for i in range(len(self.ele_power)):
             for j in range(96):
                 self.ub.append(self.ele_power[i])
                 self.lb.append(-self.ele_power[i])
-        for i in range(len(self.ele_power)):
-            for j in range(96):
-                self.ub.append(1.6 * self.ele_power[i])
-                self.lb.append(0.4 * self.ele_power[i])
         for i in range(len(self.water_power)):
             for j in range(96):
                 self.ub.append(self.water_power[i])
                 self.lb.append(-self.water_power[i])
-        for i in range(len(self.water_power)):
-            for j in range(96):
-                self.ub.append(1146.5 * 1e4)
-                self.lb.append(0)
+
+        '''
         # 等式约束建模
         self.constraint_eq = ()
         # 氢能源容量与出力关系
@@ -150,6 +133,12 @@ class AI_planing:
                                        f" x{i}_{j - 1} * 15 * 0.75 * 60 / 15 * 1e-3)"),)
         for i in range(len(self.ele_power)):
             for j in range(96):
+                # if j == 0, y{i + len(self.ele_power)}_{j} = self.ele_power[i]
+                # if j != 0, y{i + len(self.ele_power)}_{j} = y{i + len(self.ele_power)}_{j - 1}
+                # - eval(f"max(x{i}_{j - 1}, 0.9 * x{i}_{j - 1})") * 0.25,)
+                # that means we can delete the volume constraint
+                # if j != 0, y{i + len(self.ele_power)}_{j} = self.ele_power[i]
+                # - sum(max(x{i}_{j - 1}, 0.9 * x{i}_{j - 1}) for i in range(len(self.ele_power)) for j in range(1, 96)) * 0.25
                 if j == 0:
                     self.constraint_eq += (lambda x: eval(f"y{i + len(self.ele_power)}_{j}") - self.ele_power[i],)
                 else:
@@ -174,26 +163,43 @@ class AI_planing:
                           sum(eval(f"min(y{i}_{j}, 0.9 * y{i}_{j})") for i in range(len(self.ele_power))) +
                           sum(eval(f"min(z{i}_{j}, 0.75 * z{i}_{j})") for i in range(len(self.water_power))) -
                           self.loss_list[j],)
+        '''
 
         # 不等式约束建模
         self.constraint_ueq = ()
-        '''
         # 氢能源容量约束
-        for i in range(len(H2_power)):
-            for j in range(96):
-                constraint_ueq += (lambda x: -eval(f"x{i + len(H2_power)}_{j}"))
-                constraint_ueq += (lambda x: eval(f"x{i + len(H2_power)}_{j}") * 8.314 * 300 / 1000 - 14700000,)
+        for i in range(len(self.H2_power)):
+            for j in range(1, 96):
+
+                self.constraint_ueq += (lambda x: -500 + sum(eval(
+                    f"max(x{i}_{l} * 15 * 60 / 2 / 96485.33289 / 0.55 / 150 * 1e3, x{i}_{l} * 15 * 0.75 * 60 / 15 * 1e-3)")
+                    for l in range(j)),)
+
+                self.constraint_ueq += (lambda x: (500 - sum(eval(
+                    f"max(x{i}_{l} * 15 * 60 / 2 / 96485.33289 / 0.55 / 150 * 1e3, x{i}_{l} * 15 * 0.75 * 60 / 15 * 1e-3)")
+                    for l in range(j))) * 8.314 * 300 / 1000
+                                             - 14700000,)
+
         # 电化学容量约束
-        for i in range(len(ele_power)):
-            for j in range(96):
-                constraint_ueq += (lambda x: eval(f"y{i + len(ele_power)}_{j}") - 1.6 * ele_power[i],)
-                constraint_ueq += (lambda x: -eval(f"y{i + len(ele_power)}_{j}") + 0.4 * ele_power[i],)
+        for i in range(len(self.ele_power)):
+            for j in range(1, 96):
+
+                self.constraint_ueq += (lambda x: self.ele_power[i] - 0.25 * sum(eval(f"max(y{i}_{l}, 0.9 * y{i}_{l})") for l in range(j))
+                                             - 1.6 * self.ele_power[i],)
+
+                self.constraint_ueq += (lambda x: -self.ele_power[i] + 0.25 * sum(eval(f"max(y{i}_{l}, 0.9 * y{i}_{l})") for l in range(j))
+                                             + 0.4 * self.ele_power[i],)
+
         # 抽水蓄能容量约束
-        for i in range(len(water_power)):
-            for j in range(96):
-                constraint_ueq += (lambda x: eval(f"z{i + len(water_power)}_{j}") - 1146.5 * 1e4,)
-                constraint_ueq += (lambda x: -eval(f"z{i + len(water_power)}_{j}"),)
-        '''
+        for i in range(len(self.water_power)):
+            for j in range(1, 96):
+
+                self.constraint_ueq += (lambda x: self.ele_power[i] - 0.25 * 15 * 60 / 2000 / 9.8 / 1000 * 1e6 * sum(eval(f"max(z{i}_{l}, 0.75 * z{i}_{l})") for l in range(j))
+                                             - 1146.5 * 1e4,)
+
+                self.constraint_ueq += (lambda x: -self.ele_power[i] + 0.25 * 15 * 60 / 2000 / 9.8 / 1000 * 1e6 * sum(
+                    eval(f"max(z{i}_{l}, 0.75 * z{i}_{l})") for l in range(j)),)
+
 
         # 潮流缺额约束
         for j in range(96):
@@ -201,12 +207,13 @@ class AI_planing:
                 lambda x: sum(eval(f"min(x{i}_{j}, 0.55 * x{i}_{j})") for i in range(len(self.H2_power))) +
                           sum(eval(f"min(y{i}_{j}, 0.9 * y{i}_{j})") for i in range(len(self.ele_power))) +
                           sum(eval(f"min(z{i}_{j}, 0.75 * z{i}_{j})") for i in range(len(self.water_power)))
-                          - 2 * self.loss_list[j],)
+                          - 1.1 * self.loss_list[j],)
             self.constraint_ueq += (
                 lambda x: -sum(eval(f"min(x{i}_{j}, 0.55 * x{i}_{j})") for i in range(len(self.H2_power))) -
                           sum(eval(f"min(y{i}_{j}, 0.9 * y{i}_{j})") for i in range(len(self.ele_power))) -
                           sum(eval(f"min(z{i}_{j}, 0.75 * z{i}_{j})") for i in range(len(self.water_power)))
-                          - 1* self.loss_list[j],)
+                          + 0.9 * self.loss_list[j],)
+
 
         self.best_x = None
         self.best_y = None
@@ -216,17 +223,17 @@ class AI_planing:
 
     def GA_planning(self, size_pop, max_iter, prob_mut, precision):
         result = GA(func=self.obj_func, n_dim=self.n_dim, size_pop=size_pop, max_iter=max_iter, prob_mut=prob_mut,
-                    lb=self.lb, ub=self.ub, constraint_eq=self.constraint_eq, precision=precision)
+                    lb=self.lb, ub=self.ub, constraint_ueq=self.constraint_ueq, precision=precision)
         return result
 
     def PSO_planning(self, size_pop, max_iter, w, c1, c2):
         result = PSO(func=self.obj_func, n_dim=self.n_dim, pop=size_pop, max_iter=max_iter,
-                     lb=self.lb, ub=self.ub, w=w, c1=c1, c2=c2, constraint_eq=self.constraint_eq, constraint_ueq=self.constraint_ueq)
+                     lb=self.lb, ub=self.ub, w=w, c1=c1, c2=c2, constraint_ueq=self.constraint_ueq)
         return result
 
     def DE_planning(self, size_pop, max_iter, prob_mut, F):
         result = DE(func=self.obj_func, n_dim=self.n_dim, size_pop=size_pop, max_iter=max_iter, prob_mut=prob_mut, F=F,
-                    lb=self.lb, ub=self.ub, constraint_eq=self.constraint_eq)
+                    lb=self.lb, ub=self.ub, constraint_ueq=self.constraint_ueq)
         return result
 
     def SA_planning(self, T_max, T_min, L, max_stay_counter):
@@ -251,7 +258,7 @@ class AI_planing:
             best_x, best_y = result.run(times)
         best_x = np.array(best_x)
         best_x = best_x.reshape(int(self.n_dim / 96), 96)
-        if method == self.SA_planning or method == self.AFSA_planning:
+        if method == self.AFSA_planning:
             best_x = 1000 * best_x
             best_y = 1000 * best_y
         best_x = best_x.tolist()
@@ -260,8 +267,8 @@ class AI_planing:
         best_y = best_y.tolist()
         # 将best_x中的出力数据筛选出来
         best_x = best_x[0: len(self.H2_power)] + \
-                 best_x[2 * len(self.H2_power): 2 * len(self.H2_power) + len(self.ele_power)] + \
-                 best_x[2 * len(self.H2_power) + 2 * len(self.ele_power): 2 * len(self.H2_power) + 2 * len(
+                 best_x[len(self.H2_power): len(self.H2_power) + len(self.ele_power)] + \
+                 best_x[len(self.H2_power) + len(self.ele_power): len(self.H2_power) + len(
                      self.ele_power) + len(self.water_power)]
         # 筛选氢储能出力数据
         for i in range(len(self.H2_power)):
@@ -285,11 +292,10 @@ class AI_planing:
 
 
 def main():
-
     loss_list = loss_read("data/湖南_loss.csv")
     station_list = station_read("data/湖南_station.csv")
     ai_planing = AI_planing(loss_list, station_list)
-    ai_planing.calculate(ai_planing.DE_planning, 10, size_pop=50, max_iter=10, prob_mut=0.001, F=0.5)
+    ai_planing.calculate(ai_planing.GA_planning, 1, size_pop=50, max_iter=1, prob_mut=0.001, precision=1e-7)
     best_x = ai_planing.best_x
     best_y = ai_planing.best_y
     with open('data/最优解GA1.csv', 'w', newline='') as f:
